8c3f7c1a81563918464c73a98a7a2b468c04a93d
exgcdwu~ <1006605318@qq.com~>
2024-04-05 10:53:06 +0800

    temp update

--- rwgraph/_core.py
+++ rwgraph/_core.py
@@ -3,5 +3,13 @@
 
 """
 
+import xml.etree.ElementTree as et
 
+class RWGraph:
+    @classmethod
+    def init_graphfile(cls, graph_file:str)->None:
+        xmlTree:et.ElementTree = et.ElementTree(file=graph_file)
+        root:et.Element = xmlTree.getroot()
+        cls.map_default_properties = root.attrib()
+        cls.map_optional_properties = root.pro
 
