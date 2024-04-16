e12bfde727151515f91f9770735dc2a496caec3c
exgcdwu <135420013+exgcdwu@users.noreply.github.com>
2024-04-17 00:21:32 +0800

    try to add requirements

--- setup.py
+++ setup.py
@@ -4,25 +4,26 @@ import os
 from setuptools import setup, find_packages
 
 __version__ = '1.2.1'
-current_dir = os.path.dirname(__file__)
-requirements = open(current_dir + '/requirements.txt').readlines()
 
 def readme():
     with open('README.md') as file:
         return file.read()
 
-strreadme = readme()
+def requirements():
+    with open('requirements.txt') as file:
+        return file.readlines()
 
 setup(
     name = 'rwmapeditor_exgcdwu',
     version = __version__,
     author = 'exgcdwu',
     author_email = '1006605318@qq.com',
+    license = 'GPL-3.0',
     url = "https://github.com/exgcdwu/Rusted-Warfare-map-editor-for-city-occupation-play-",
-    long_description = strreadme,
+    long_description = readme(),
     long_description_content_type = "text/markdown",
     packages = find_packages(exclude=["tests"]),
     data_files = {('.', './requirements.txt')},
     python_requires = '>=3.0.0',
-    install_requires = requirements
+    install_requires = requirements()
 )
