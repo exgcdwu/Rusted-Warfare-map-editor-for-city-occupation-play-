898602213144bb843c82d10058c04b3d16f882bf
exgcdwu~ <1006605318@qq.com~>
2024-12-26 21:01:02 +0800

    update 1.7.9.8 for testing

--- command/__init__.py
+++ command/__init__.py
@@ -4,3 +4,4 @@ from command.tsindep._core import auto_func as tsindep
 from command.layerauto._core import auto_func as layerauto
 from command.layerobauto._core import auto_func as layerobauto
 from command.tilesetauto._core import auto_func as tilesetauto
+from command.resizeauto._core import auto_func as resizeauto
